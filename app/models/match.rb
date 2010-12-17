34da5377e29f0da6f0de62d38e6ac65d5c3ce442
Cameron Dykes <yellow5c@gmail.com>
2010-12-17 08:47:33 -0600

    Use single quotes around team color condition

--- app/models/match.rb
+++ app/models/match.rb
@@ -5,8 +5,8 @@ class Match < ActiveRecord::Base
   has_many :match_players
   
   has_many :players,      :through => :match_players
-  has_many :red_players,  :through => :match_players, :conditions => 'match_players.team = "red"' , :source => :player
-  has_many :blue_players, :through => :match_players, :conditions => 'match_players.team = "blue"', :source => :player
+  has_many :red_players,  :through => :match_players, :conditions => "match_players.team = 'red'" , :source => :player
+  has_many :blue_players, :through => :match_players, :conditions => "match_players.team = 'blue'", :source => :player
   
   has_many :stats, :class_name => "MatchStat"
   
