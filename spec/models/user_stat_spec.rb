6f605fe82e6408ee188dbb674fab8b1d0a31dca5
Cameron Dykes <yellow5c@gmail.com>
2011-09-20 20:08:29 -0500

    Add specs for UserStat scopes

--- spec/models/user_stat_spec.rb
+++ spec/models/user_stat_spec.rb
@@ -14,4 +14,48 @@ describe UserStat do
     it { should belong_to(:user) }
     it { should belong_to(:other_user) }
   end
+
+  describe 'scopes' do
+    context 'opponents and allies scopes' do
+      let!(:opponent_user_stat) { Fabricate(:user_stat, :relation => 'opponent') }
+      let!(:ally_user_stat) { Fabricate(:user_stat, :relation => 'ally') }
+
+      context 'opponents' do
+        subject { UserStat.opponents }
+
+        it "returns records with :relation => 'opponent'" do
+          subject.should eq([opponent_user_stat])
+        end
+      end
+
+      context 'allies' do
+        subject { UserStat.allies }
+
+        it "returns records with :relation => 'ally'" do
+          subject.should eq([ally_user_stat])
+        end
+      end
+    end
+
+    context 'won and lost scopes' do
+      let!(:won_user_stat) { Fabricate(:user_stat, :won => true) }
+      let!(:lost_user_stat) { Fabricate(:user_stat, :won => false) }
+
+      context 'won' do
+        subject { UserStat.won }
+
+        it 'returns records with :won => true' do
+          subject.should eq([won_user_stat])
+        end
+      end
+
+      context 'lost' do
+        subject { UserStat.lost }
+
+        it 'returns records with :won => false' do
+          subject.should eq([lost_user_stat])
+        end
+      end
+    end
+  end
 end
