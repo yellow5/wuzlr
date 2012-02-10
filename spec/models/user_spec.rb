b094a2a6d7daac37785492555ec680a8a9ccf935
Cameron Dykes <yellow5c@gmail.com>
2012-02-09 19:45:29 -0600

    Add reset_password_sent_at to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,7 @@ describe User do
     it { should have_db_column(:email).of_type(:string).with_options(:default => '', :null => false) }
     it { should have_db_column(:encrypted_password).of_type(:string).with_options(:limit => 128, :default => '', :null => false) }
     it { should have_db_column(:reset_password_token).of_type(:string) }
+    it { should have_db_column(:reset_password_sent_at).of_type(:datetime) }
     it { should have_db_column(:remember_token).of_type(:string) }
     it { should have_db_column(:remember_created_at).of_type(:datetime) }
     it { should have_db_column(:sign_in_count).of_type(:integer).with_options(:default => 0) }
